59a5a2cee4a7af696da3cb47e5e3fed25393feef
Shanthi Pendleton <shanthi.pendleton@neos.co.uk>
2017-11-26 17:53:45 +0000

    Add stable model

--- apps/legacy/test/legacy/accounts/accounts_test.exs
+++ apps/legacy/test/legacy/accounts/accounts_test.exs
@@ -4,23 +4,38 @@ defmodule Legacy.AccountsTest do
   import Legacy.Factory
 
   alias Legacy.Accounts
-  alias Legacy.Accounts.User
+  alias Legacy.Accounts.{User, Stable}
 
   @invalid_attrs %{Email: nil}
 
-  def fixture(:user, attrs \\ params_for(:user)) do
+  def user_fixture(attrs \\ params_for(:user)) do
     {:ok, user} = Accounts.create_user(attrs)
     user
   end
 
+  def stable_fixture(attrs \\ params_for(:stable)) do
+    {:ok, stable} = Accounts.create_stable(attrs)
+    stable
+  end
+
   test "list_users/1 returns all users" do
-    user = fixture(:user)
+    user = user_fixture()
     assert Accounts.list_users() == [user]
   end
 
   test "get returns the user with given id" do
-    user = fixture(:user)
+    user = user_fixture()
     assert Accounts.get(user."ID") == user
+  end
+
+  test "get by email returns the user with given address" do
+    user = user_fixture()
+    assert Accounts.get_by(%{"Email" => user."Email"}) == user
+  end
+
+  test "get by stable returns the user with given name" do
+    stable = stable_fixture()
+    assert Accounts.get_by(%{"StableName" => stable."StableName"}) == stable
   end
 
   test "create_user/1 with valid data creates a user" do
@@ -34,7 +49,7 @@ defmodule Legacy.AccountsTest do
   end
 
   test "update_user/2 with valid data updates the user" do
-    user = fixture(:user)
+    user = user_fixture()
     update_attrs = params_for(:user)
     assert {:ok, user} = Accounts.update_user(user, update_attrs)
     assert %User{} = user
@@ -42,19 +57,19 @@ defmodule Legacy.AccountsTest do
   end
 
   test "update_user/2 with invalid data returns error changeset" do
-    user = fixture(:user)
+    user = user_fixture()
     assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
     assert user == Accounts.get(user."ID")
   end
 
   test "delete_user/1 deletes the user" do
-    user = fixture(:user)
+    user = user_fixture()
     assert {:ok, %User{}} = Accounts.delete_user(user)
     refute Accounts.get(user."ID")
   end
 
   test "change_user/1 returns a user changeset" do
-    user = fixture(:user)
+    user = user_fixture()
     assert %Ecto.Changeset{} = Accounts.change_user(user)
   end
 
