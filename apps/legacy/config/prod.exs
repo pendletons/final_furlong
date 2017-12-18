79508519671a456944b10aad9a0808d5103edd98
Shanthi Pendleton <pendletons@users.noreply.github.com>
2017-12-18 23:18:07 +0000

    Fix typo (#13)

--- apps/legacy/config/prod.exs
+++ apps/legacy/config/prod.exs
@@ -18,7 +18,7 @@ config :legacy, LegacyWeb.Endpoint,
   url: [schema: "https", host: "finalfurlongapi.herokuapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
